da128304840f807f3ccecae489769facc823a572
Ragil Murdiantoro <ragil>
2023-11-09 17:43:24 +0700

    fix init cluster app and load test script

--- kubernetes/init_cluster.sh
+++ kubernetes/init_cluster.sh
@@ -7,14 +7,14 @@ gcloud beta container --project "rich-aspect-403310" clusters create "documenso-
     --disk-type "pd-standard" \
     --disk-size "30" \
     --num-nodes "2" \
-    --zone "asia-southeast1-a" \
-    --node-locations "asia-southeast1-a" \
+    --zone "asia-southeast2-b" \
+    --node-locations "asia-southeast2-b" \
     --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
     --logging=SYSTEM,WORKLOAD \
     --monitoring=SYSTEM,STORAGE,POD,DEPLOYMENT,STATEFULSET,HPA \
     --enable-ip-alias \
     --network "projects/rich-aspect-403310/global/networks/default" \
-    --subnetwork "projects/rich-aspect-403310/regions/asia-southeast1/subnetworks/default" \
+    --subnetwork "projects/rich-aspect-403310/regions/asia-southeast2/subnetworks/default" \
     --no-enable-intra-node-visibility \
     --default-max-pods-per-node "110" \
     --no-enable-master-authorized-networks \
@@ -25,7 +25,7 @@ gcloud compute disks create gke-pv-disk \
     --project=rich-aspect-403310 \
     --type=pd-standard \
     --size=50GB \
-    --zone=asia-southeast1-a
+    --zone=asia-southeast2-b
 
 # Reservate static public ip
 gcloud compute addresses create documenso-public-ip --project=rich-aspect-403310 --global
